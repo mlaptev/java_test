84bec1207c07a6bf701fce30451c33f2ed56bc67
mlaptev <mlaptev@issart.com>
2019-03-28 15:10:50 +0600

    Решение задачи №2

--- sandbox/src/main/java/ru/stqa/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/test/sandbox/MyFirstProgram.java
@@ -3,25 +3,27 @@ package ru.stqa.test.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-	System.out.println("Hello, world!");
+		hello("world");
+		hello("user");
+		hello("Test");
 
-	System.out.println(2 + 2);
-	System.out.println(2 * 2);
-	System.out.println(2 / 2);
-	System.out.println(2 - 2);
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + (l));
+	}
+	public static void hello (String somebody) {
 
-		System.out.println(1 / 2);
-		System.out.println(1.0 / 2);
-		System.out.println(1 / 2.0);
-		System.out.println(2.0 / 2);
 
-		System.out.println("2" + "2");
-		System.out.println("2" + 2);
-		System.out.println(2 + "2");
 
-		System.out.println(2 + 2 * 2);
+
+		System.out.println("Hello, " + somebody + "!");
+
+
 
 		System.out.println("2 + 2 = " + (2 + 2));
+
+		double l = 8.0;
+		double s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 
 
 
