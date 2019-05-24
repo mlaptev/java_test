03f02f4ccd16bd90ff566ecf068b383d837cf0f1
mlaptev <mlaptev@issart.com>
2019-05-24 16:01:24 +0600

    Задание 2 исправление

--- sandbox/src/main/java/ru/stqa/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/MyFirstProgram.java
@@ -16,6 +16,34 @@ public class MyFirstProgram {
 		double l = 8.0;
 		double s = l * l;
 		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+		Point p1 = new Point(2, 0);
+		Point p2 = new Point(8, 0);
+
+		double distance = p2.distance(p1);
+		System.out.println(distance);
+
+		Point p3 = new Point(3, 9);
+		Point p4 = new Point(1, 5);
+
+		double distance1 = p4.distance(p3);
+		System.out.println(distance1);
+
+		Point p5 = new Point(5, 9);
+		Point p6 = new Point(-2, 3);
+
+		double distance2 = distance(p5, p6);
+		System.out.println(distance2);
 	}
-	
+
+	public static double distance(Point p1, Point p2) {
+		double dx = p2.x - p1.x;
+		double dy = p2.y - p1.y;
+		return Math.sqrt(dx*dx + dy*dy);
+	}
+
+	}
+
+
+
+
 }
--- sandbox/src/main/java/ru/stqa/Point.java
+++ sandbox/src/main/java/ru/stqa/Point.java
@@ -2,15 +2,17 @@ package ru.stqa;
 
 public class Point {
 
-        public double x;
-        public double y;
+    private double x;
+    private double y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    public double distance(Point p) {
-    return Math.sqrt(((p.x- this.x) * (p.x - this.x)) + ((p.y - this.y) * (p.y - this.y)));
+
+    public double distance (Point p2) {
+        return Math.sqrt(((p2.x - this.x) * (p2.x - this.x)) + ((p2.y - this.y) * (p2.y - this.y)));
     }
+
 }
 
