3f7ba048fd1840cf0b584fe05657a3dd24061b45
mlaptev <mlaptev@issart.com>
2019-10-09 17:19:03 +0600

    Модуль для тестов Mantis

---
+++ Mantist-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.mantis.appmanager.ApplicationManager;
+
+
+public class TestBase {
+
+
+
+    public static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", org.openqa.selenium.remote.BrowserType.CHROME));
+
+//    @BeforeMethod
+    @BeforeSuite
+    public void setUp() throws Exception { app.init(); }
+
+//    @AfterClass(alwaysRun = true)
+    @AfterSuite
+    public void tearDown() { app.stop(); }
+
+
+}
